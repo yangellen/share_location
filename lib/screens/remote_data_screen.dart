a6be0dc096e40003cdb3e1213be957287125820f
Ellen Yang <yange@oregonstate.edu>
2021-03-02 12:43:56 -0800

    request api

--- lib/screens/remote_data_screen.dart
+++ lib/screens/remote_data_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_practice_wk8/models/character.dart';
 import 'package:http/http.dart' as http;
@@ -6,7 +8,7 @@ class RemoteDataScreen extends StatefulWidget {
   final title;
   RemoteDataScreen({this.title});
 
-  static const URL = 'https://swapi.dev/aip/people/1';
+  static const URL = 'https://swapi.dev/api/people/1';
 
   @override
   _RemoteDataScreenState createState() => _RemoteDataScreenState();
@@ -16,7 +18,31 @@ class _RemoteDataScreenState extends State<RemoteDataScreen> {
   Character character;
 
   @override
+  void initState() {
+    super.initState();
+    retrieveCharacterData();
+  }
+
+  //getting info from aip
+  void retrieveCharacterData() async {
+    final http.Response apiResponse =
+        await http.get(RemoteDataScreen.URL); //will result in string
+
+    character = Character.fromJSON(jsonDecode(apiResponse.body));
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(child: CircularProgressIndicator());
+    if (character == null) {
+      return Center(child: CircularProgressIndicator());
+    } else {
+      return Center(
+        child: Text(
+          '${character.name}',
+          style: Theme.of(context).textTheme.headline5,
+        ),
+      );
+    }
   }
 }
